61c53e7912efd57399c66c031357d91557515ffa
tim-tang <tang.jilong@139.com>
2011-11-23 10:17:33 +0800

    generate model templates

--- lib/generators/crawler_engine_generator.rb
+++ lib/generators/crawler_engine_generator.rb
@@ -8,12 +8,16 @@ class CrawlerEngineGenerator < Rails::Generators::Base
 
 	desc "Generates migration for crawler engine model"
 	def self.source_root
-		#File.join(File.dirname(__FILE__), 'templates')
 		File.expand_path('../templates', __FILE__)
 	end
 
 	def create_migration_file
 		migration_template 'migration.rb', 'db/migrate/create_crawler_engine'
+	end
+
+	def crate_model_file
+		template 'post.rb', 'app/models/post.rb'
+		template 'source.rb', 'app/models/source.rb'
 	end
 end
 
---
+++ lib/generators/templates/source.rb
@@ -0,0 +1,4 @@
+class Source < ActiveRecord::Base
+
+end
+
