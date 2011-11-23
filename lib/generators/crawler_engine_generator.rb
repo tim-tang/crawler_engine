8fe2c88d04e42575a7d0105a2f3f836b8ba4609d
tim-tang <tang.jilong@139.com>
2011-11-23 09:36:34 +0800

    support rails3

--- lib/generators/crawler_engine_generator.rb
+++ lib/generators/crawler_engine_generator.rb
@@ -2,7 +2,7 @@ require 'rails/generators'
 require 'rails/generators/migration'
 require 'rails/generators/active_record'
 
-class CrawlerEngineGenerator < Rails::Generator::Base
+class CrawlerEngineGenerator < Rails::Generators::Base
 	include Rails::Generators::Migration
 	extend ActiveRecord::Generators::Migration
 
