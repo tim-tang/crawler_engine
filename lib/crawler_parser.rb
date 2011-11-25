b87ed964941821f9fc56923bb40eb696cf4d9dc1
tim-tang <tang.jilong@139.com>
2011-11-25 22:58:55 +0800

    add judgement to fetch newest posts

--- lib/crawler_parser.rb
+++ lib/crawler_parser.rb
@@ -1,5 +1,6 @@
 #encoding:utf-8
 require File.expand_path('../post',__FILE__)
+require File.expand_path('../html_gen',__FILE__)
 require 'nokogiri'
 require 'rest-open-uri'
 require 'simple-rss'
@@ -13,6 +14,7 @@ class CrawlerParser
 
 	def parse_rss(sources)
 		return if sources.nil? or sources.size.eql?(0)
+		@html_gen=HtmlGen.new
 		#logger.info("Links to crawl >>"+links.to_s)
 		puts "Links to crawl >>"+sources.to_s
 		sources.uniq!
@@ -24,32 +26,35 @@ class CrawlerParser
 				#logger.info("SimpleRSS got unexpected error, rss exit")
 				puts ex
 			end
-			#TODO:
-			#puts rss.feed_tags.title
-			#puts rss.feed_tags.description
 
 			for item in rss.items
-				print_rss_item(item)
-				#parse post details
-				begin
-					doc = Nokogiri::HTML open(item.link.to_s)
-				rescue Exception=>ex
-					#logger.error(ex)
-					#logger.info("Nokogiri got unexpected error")
-					puts ex
-				end
-				return unless doc
-				doc.xpath(source.filter).each do |content|
-					#puts content.to_s.force_encoding('GB2312')
-					#Iconv.iconv("GB2312//IGNORE","UTF-8//IGNORE", content)
-					#Iconv.iconv("UTF-8//IGNORE","GB2312//IGNORE", content)
-					Post.create(
-						:title=>item.title.to_s.force_encoding('UTF-8'),
-						:source=>item.link.to_s.force_encoding('UTF-8'),
-						:content=>content.to_s.force_encoding('UTF-8'),
-						:published_at=>item.pubDate,
-						:site_name=>source.site_name,
-						:category => source.category)
+				#print_rss_item(item)
+				puts "Post published date >>" + item.pubDate.to_s
+				puts "Crawler updated date >>" + source.crawled_at.to_s
+				if item.pubDate >= source.crawled_at
+					print_rss_item(item)
+					begin
+						doc = Nokogiri::HTML open(item.link.to_s)
+					rescue Exception=>ex
+						#logger.error(ex)
+						#logger.info("Nokogiri got unexpected error")
+						puts ex
+					end
+					return unless doc
+					doc.xpath(source.filter).each do |content|
+						#puts content.to_s.force_encoding('GB2312')
+						#Iconv.iconv("GB2312//IGNORE","UTF-8//IGNORE", content)
+						#Iconv.iconv("UTF-8//IGNORE","GB2312//IGNORE", content)
+						@post = Post.new
+						@post.title=item.title.to_s.force_encoding('UTF-8')
+						@post.source=item.link.to_s.force_encoding('UTF-8')
+						@post.content=content.to_s.force_encoding('UTF-8')
+						@post.published_at=item.pubDate
+						@post.site_name=source.site_name
+						@post.category=source.category
+						@post.save(@post)
+						@html_gen.generate(@post)
+					end
 				end
 			end
 			#update crawler exec timestamp
