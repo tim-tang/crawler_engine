365e9e02cd63ecea751c9dee1a5366915bb84cd5
tim-tang <tang.jilong@139.com>
2011-11-27 14:35:24 +0800

    alter to multi-thread for crawler

--- lib/crawler_parser.rb
+++ lib/crawler_parser.rb
@@ -18,47 +18,54 @@ class CrawlerParser
 		#logger.info("Links to crawl >>"+links.to_s)
 		puts "Links to crawl >>"+sources.to_s
 		sources.uniq!
+		threads=[]
 		sources.each do |source|
-			begin
-				rss = SimpleRSS.parse open(source.link)
-			rescue Exception=>ex
-				#logger.error(ex)
-				#logger.info("SimpleRSS got unexpected error, rss exit")
-				puts ex
-			end
+			threads << Thread.new do
+				puts "Create thread for num >>"+source.id.to_s
+				begin
+					rss = SimpleRSS.parse open(source.link)
+				rescue Exception=>ex
+					#logger.error(ex)
+					#logger.info("SimpleRSS got unexpected error, rss exit")
+					puts ex
+				end
 
-			for item in rss.items
-				#print_rss_item(item)
-				puts "Post published date >>" + item.pubDate.to_s
-				puts "Crawler updated date >>" + source.crawled_at.to_s
-				if item.pubDate >= source.crawled_at
-					print_rss_item(item)
-					begin
-						doc = Nokogiri::HTML open(item.link.to_s)
-					rescue Exception=>ex
-						#logger.error(ex)
-						#logger.info("Nokogiri got unexpected error")
-						puts ex
-					end
-					return unless doc
-					doc.xpath(source.filter).each do |content|
-						#puts content.to_s.force_encoding('GB2312')
-						#Iconv.iconv("GB2312//IGNORE","UTF-8//IGNORE", content)
-						#Iconv.iconv("UTF-8//IGNORE","GB2312//IGNORE", content)
-						@post = Post.new
-						@post.title=item.title.to_s.force_encoding('UTF-8')
-						@post.source=item.link.to_s.force_encoding('UTF-8')
-						@post.content=content.to_s.force_encoding('UTF-8')
-						@post.published_at=item.pubDate
-						@post.site_name=source.site_name
-						@post.category=source.category
-						@post.save(@post)
-						@html_gen.generate(@post)
+				for item in rss.items
+					#print_rss_item(item)
+					#puts "Post published date >>" + item.pubDate.to_s
+					#puts "Crawler updated date >>" + source.crawled_at.to_s
+					if item.pubDate >= source.crawled_at
+						print_rss_item(item)
+						begin
+							doc = Nokogiri::HTML open(item.link.to_s)
+						rescue Exception=>ex
+							#logger.error(ex)
+							#logger.info("Nokogiri got unexpected error")
+							puts ex
+						end
+						return unless doc
+						doc.xpath(source.filter).each do |content|
+							#Iconv.iconv("GB2312//IGNORE","UTF-8//IGNORE", content)
+							#Iconv.iconv("UTF-8//IGNORE","GB2312//IGNORE", content)
+							@post = Post.new
+							@post.title=item.title.to_s.force_encoding('UTF-8')
+							@post.source=item.link.to_s.force_encoding('UTF-8')
+							@post.content=content.to_s.force_encoding('UTF-8')
+							@post.published_at=item.pubDate
+							@post.site_name=source.site_name
+							@post.category=source.category
+							@post.save(@post)
+							@html_gen.generate(@post)
+						end
 					end
 				end
+				#update crawler exec timestamp
+				source.update_attribute("crawled_at", Time.now)
 			end
-			#update crawler exec timestamp
-			source.update_attribute("crawled_at", Time.now)
+		end
+		# thread end stop treads finally.
+		threads.each do |thread|
+			thread.join
 		end
 	end
 
