b885a13d369b0df00809166fce61beaa1fd5b16a
tim-tang <tang.jilong@139.com>
2011-11-28 19:10:42 +0800

    integrate with rails proj

--- lib/html_gen.rb
+++ lib/html_gen.rb
@@ -11,21 +11,20 @@ class HtmlGen
 
 	def generate(post)
 		date=post.published_at.strftime("%Y%m%d")
-		#file_dir="#{RAILS_ROOT}/public/html_contetns"
-		file_dir = File.expand_path('../public/html_contents', __FILE__)
-		if File.exit?(file_dir)
+		file_dir="#{RAILS_ROOT}/public/html_contetns"
+		#file_dir = File.expand_path('../public/html_contents', __FILE__)
+		if File.exist?(file_dir)
 			puts "File directory already exists..."
 		else
 			Dir.mkdir(file_dir)
-			if File.exist?(file_dir.concat("/"+date))
-				puts "Sub File exits..."
-			else
-				Dir.mkdir(file_dir)
-			end
+		end
+		if File.exist?(file_dir.concat("/"+date))
+			puts "Sub File exits..."
+		else
+			Dir.mkdir(file_dir)
 		end
 		begin
 			puts "Generating html for post >>"+post.title
-			#file_dir = File.expand_path('../public/html_contents', __FILE__)
 			file_name = file_dir+"/"+post.id.to_s+".html"
 			htmlFile = File.new(file_name, "w+")
 			htmlFile.puts "<html><header><title>Everyday News</title><meta http-equiv=Content-Type content=text/html;charset=utf-8></header>"
